52f74727c8b836a49ae999564fd35855a8e2cf2a
Jean Rothstein <jean.machado@sou.inteli.edu.br>
2023-10-02 12:16:21 -0300

    Update index.jsx

--- ponderada4/frontend/pages/Streamlit/index.jsx
+++ ponderada4/frontend/pages/Streamlit/index.jsx
@@ -18,7 +18,7 @@ export default function RedirectStreamlit() {
     <div>
       {token && (
         <iframe
-          src={`http://54.163.165.25:8501/?token=${token}`} // Substitua pela URL real do servidor Streamlit
+          src={`http://54.163.165.25/?token=${token}`} // Substitua pela URL real do servidor Streamlit
           width="100%"
           height="800px"
           title="Streamlit App"
